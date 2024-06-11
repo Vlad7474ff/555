98e22884a3e82bceb78515ae2c029bb4bf6a923c
vlad2 <r.mamo.r@mail.ru>
2024-06-11 23:40:16 +0300

    test2

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_random_hash(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters_and_digits) for i in range(length))
+
+random_hash = generate_random_hash(6)
+print(random_hash)
+#!
