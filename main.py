f09f739fddb045cbf0d9b79221be16962aa185ed
vlad2 <r.mamo.r@mail.ru>
2024-06-12 13:33:49 +0300

    test5

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import random
-import string
-k=6
-def generate_random_hash(length):
-    letters_and_digits = string.ascii_letters + string.digits
-    return ''.join(random.choice(letters_and_digits) for i in range(length))
 
-random_hash = generate_random_hash(k)
-print(random_hash)
+k=5+1
+print(k)
+
