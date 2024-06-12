9b725e0c1a7d3e22d97715116e0370eb88f0f2c8
vlad2 <r.mamo.r@mail.ru>
2024-06-12 13:09:44 +0300

    test5

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 import string
-k=7
+k=6
 def generate_random_hash(length):
     letters_and_digits = string.ascii_letters + string.digits
     return ''.join(random.choice(letters_and_digits) for i in range(length))
